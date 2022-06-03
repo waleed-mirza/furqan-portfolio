da73c4e486b44f2d1ae35058b67776e1a134c368
waleed mirza <waleedmirza667@gmail.com>
2022-06-03 23:36:09 +0500

    scroll animation and footer done

--- js/index.js
+++ js/index.js
@@ -22,3 +22,46 @@ $(".open-menu").on("click", function () {
     { duration: 1, opacity: 0, y: -200 }
   );
 });
+
+// animation on scroll
+const scrollElements = document.querySelectorAll(
+  ".transition-left, .transition-right, .fade-in"
+);
+
+const elementInView = (el, dividend = 1) => {
+  const elementTop = el.getBoundingClientRect().top;
+
+  return (
+    elementTop <=
+    (window.innerHeight || document.documentElement.clientHeight) / dividend
+  );
+};
+const elementOutofView = (el) => {
+  const elementTop = el.getBoundingClientRect().top;
+
+  return (
+    elementTop > (window.innerHeight || document.documentElement.clientHeight)
+  );
+};
+
+const displayScrollElement = (element) => {
+  element.classList.add("scroll-transition");
+};
+
+const hideScrollElement = (element) => {
+  element.classList.remove("scroll-transition");
+};
+
+const handleScrollAnimation = () => {
+  scrollElements.forEach((el) => {
+    if (elementInView(el, 1.25)) {
+      displayScrollElement(el);
+    } else if (elementOutofView(el)) {
+      hideScrollElement(el);
+    }
+  });
+};
+
+window.addEventListener("scroll", () => {
+  handleScrollAnimation();
+});
